2d65c84d0587b99d1fed2983e15c3cf4db936768
cloudy <1197269596@qq.com>
2017-08-18 08:06:36 +0800

    ajax & jsonp

---
+++ ajax & jsonp/index.js
@@ -0,0 +1,116 @@
+// ajajx: url, type, data, success, error
+// jsonp: jsonp
+var request = function (json) {
+  json = json || {}
+
+  json.data = json.data || {}
+  var params = json.jsonp ? jsonp(params) : ajax(params)
+
+  // ajax 请求
+  var ajax = function (params) {
+    params.type = json.type.toUpperCase() || 'GET'
+    params.data = formatParams(params.data)
+
+    var xhr
+
+    if (window.XMLHttpRequest) {
+      xhr = new XMLHttpReuest()
+    } else {
+      xhr = new ActiveXObject('Microsoft.XMLHTTP')
+    }
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4) {
+        var status = xhr.status
+
+        // 状态响应码 在 200 和 300 之间
+        if (status >= 200 && status < 300) {
+          var response = ''
+
+          var type = xhr.getResponseHeader('Content-type')
+
+          if (type.indexOf('xml') !== -1 && xhr.responseXML) {
+            response = xhr.responseXML
+          } else if (type === 'application/json') {
+            response = JSON.parse(xhr.responseText)
+          } else {
+            response = xhr.responseText
+          }
+
+          // 成功回调
+          params.success && params.success(response)
+        } else {
+          // 失败回调
+          params.error && params.error(status)
+        }
+      }
+    }
+
+    if (params.type === 'GET') {
+      xhr.open(params.type, params.url + '?' + params.data, true)
+
+      xhr.send(null)
+    } else {
+      xhr.open(params.type, params.url)
+
+      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8')
+
+      xhr.send(params.data)
+    }
+  }
+
+  // jsonp 请求
+  var jsonp = function (params) {
+    var callbackName = params.jsonp
+
+    var script = document.createElement('script')
+
+    var head = document.getElementsByTagName('head')[0]
+
+    // 设置传递给后台的回调参数名
+    params.data['callback'] = callbackName
+
+    var data = formatParams(params.data)
+
+    head.appendChild(script)
+
+    // 创建jsonp回调函数
+    window[callbackName] = function (result) {
+      head.removeChild(script)
+
+      clearTimeout(script.timer)
+
+      window.callbackName = null
+
+      params.success && params.success(result)
+    }
+
+    script.src = params.url + '?' + data
+
+    // 超时处理
+    var time = params.time || 500
+
+    script.timer = setTimeout(function () {
+      window[callbackName] = null
+
+      head.removeChild(script)
+
+      params.error && params.error({
+        message: 'timeout'
+      })
+    }, time)
+  }
+}
+
+function formatParams (data) {
+  var arr = []
+
+  for (var i in data) {
+    arr.push(encodeURLComponent(i) + '=' + encodeURIComponent(data[i]))
+  }
+
+  // 添加随机数
+  arr.push('v=' + Math.floor(Math.random() * 10000 + 100))
+
+  return arr.join('&')
+}
